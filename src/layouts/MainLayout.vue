cb304943c71136ff5d0c50d6f2f42a074035dbbb
Ayi Solahudin <ayiis.218@gmail.com>
2026-02-10 15:31:33 +0700

    feat: update MainLayout with navigation links and add auth store for user authentication

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,16 +1,19 @@
 <script lang="ts" setup>
+import { UserIcon } from '@heroicons/vue/24/outline';
 import { RouterView } from 'vue-router'
+
+const session = false
 </script>
 
 <template>
-  <div class="w-full flex flex-row bg-gray-300 shadow-md">
+  <div class="w-full flex flex-row items-center bg-gray-300 shadow-md">
     <nav class="w-full flex flex-row justify-center gap-3 py-3 cursor-pointer">
       <RouterLink to="/" class="text-black font-normal">Home</RouterLink>
       <RouterLink to="/about" class="text-black font-normal">About</RouterLink>
       <RouterLink to="/anime" class="text-black font-normal">Anime</RouterLink>
       <RouterLink to="/otp" class="text-black font-normal">OTP</RouterLink>
     </nav>
-    <div class="flex flex-row justify-end gap-1 my-2 mr-4">
+    <div v-if="session" class="flex flex-row justify-end gap-1 my-2 mr-4">
       <RouterLink
         to="/login"
         class="w-[80px] text-sm text-white font-normal border bg-blue-500 p-2 hover:bg-blue-400 rounded-md text-center"
@@ -22,6 +25,11 @@ import { RouterView } from 'vue-router'
         class="w-[80px] text-sm text-blue-500 font-normal border bg-white border-2 border-blue-500 p-2 hover:bg-blue-100 rounded-md text-center"
       >
         Register
+      </RouterLink>
+    </div>
+    <div v-else class="flex flex-row justify-end mr-3 ">
+      <RouterLink to="/profile" class="border border-black rounded-full p-1 cursor-pointer">
+        <UserIcon class="w-4 h-4" />
       </RouterLink>
     </div>
   </div>
---
+++ src/stores/auth.ts
@@ -0,0 +1,7 @@
+export const useAuthStore = async () => {
+  const token = await cookieStore.get('token')
+
+  return {
+    isAuthenticated: token,
+  }
+}
