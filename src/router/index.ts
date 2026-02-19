309a3a928907a38cbf372d2eab35fa962dbdee51
Ayi Solahudin <ayiis.218@gmail.com>
2026-02-19 09:21:17 +0700

    feat: Create MainView component and configure it as the home route.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MainView from '../views/MainView.vue'
 import AboutView from '../views/AboutView.vue'
 import AnimeView from '../views/AnimeView.vue'
 import OtpView from '../views/OtpView.vue'
@@ -16,7 +16,7 @@ const router = createRouter({
       path: '/',
       component: MainLayout,
       children: [
-        { path: '/', name: 'home', component: HomeView },
+        { path: '/', name: 'home', component: MainView },
         { path: '/about', name: 'about', component: AboutView },
         { path: '/anime', name: 'anime', component: AnimeView },
         { path: '/otp', name: 'otp', component: OtpView },
---
+++ src/views/MainView.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="w-full h-full p-3 flex flex-col gap-3">
+        <div class="w-full flex flex-row items-center gap-3 border border-gray-300 p-3 rounded-md">
+            <div class="flex-1 p-3">
+                <div class="w-20 h-20 rounded-full overflow-hidden">
+                    <img src="" alt="profile" width="100%" height="100%" class="object-cover"/>
+                </div>
+            </div>
+            <div class="flex-1 flex flex-col gap-3 border border-gray-300 p-3 rounded-md">
+                <h1 class="text-xl font-normal font-bold">Ayi Solahudin</h1>
+                <p class="text-sm font-normal text-gray-500">As a Fullstack Developer, 3 years as a Frontend Developer in PT. Bank Rakyat Indonesia (Persero) Tbk.</p>
+            </div>  
+        </div>
+        <div class="w-full flex flex-row gap-3 border border-gray-300 p-3 rounded-md">
+            <div class="w-full flex flex-row gap-3">
+                <div class="w-full flex-1 border border-gray-300 p-3 rounded-md">
+                    <h1 class="text-xl font-normal font-bold">Main View</h1>
+                </div>
+                <div class="w-full flex-1 border border-gray-300 p-3 rounded-md">
+                    <h1 class="text-xl font-normal font-bold">Main View</h1>
+                </div>
+            </div>
+        </div>
+        <div class="w-full flex flex-row gap-3 border border-gray-300 p-3 rounded-md">
+            <div class="w-full flex flex-row gap-3">
+                <div class="w-full flex-1 border border-gray-300 p-3 rounded-md">
+                    <h1>Main View</h1>
+                </div>
+                <div class="w-full flex-1 border border-gray-300 p-3 rounded-md">
+                    <h1>Main View</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
