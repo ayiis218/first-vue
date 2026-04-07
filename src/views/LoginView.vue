ce92946b0c21c23e02b061b9e6be6d09d406911d
Ayi Solahudin <shallahudin.ayi@gmail.com>
2026-04-07 14:46:49 +0700

    update: auth page condition

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -44,79 +44,74 @@ const useAppleLogin = () => {
 </script>
 
 <template>
-
-  <div class="w-full h-full flex flex-row justify-center items-center">
-    <div class="h-full flex-1 ">
-      <div class="w-full h-full flex flex-col justify-center items-center">
-        <img src="../assets/login.jpg" alt="login" width="600" height="600"/>
-      </div>
+  <div class="h-screen w-full bg-slate-950 flex flex-row text-slate-200 selection:bg-indigo-500/30 font-sans">
+    <div class="w-full h-full flex flex-col justify-center items-center">
+      <img src="../assets/login.jpg" alt="login" class="w-full h-full object-cover"/>
     </div>
-    <div class="h-full flex-1 bg-blue-300 ">
-      <div class="h-full flex flex-col gap-3 justify-center items-center">
-        <div class="w-[60%] flex flex-col gap-3 p-3">
-          <h1 class="text-2xl font-bold text-center">Form Login</h1>
-          <form @submit.prevent="onLogin" class="w-full flex flex-col gap-3">
+    <div class="w-full h-full flex flex-col justify-center items-center bg-blue-300">
+      <div class="w-[60%] flex flex-col gap-3 p-3">
+        <h1 class="text-2xl font-bold text-center text-white mb-4">Form Login</h1>
+        <form @submit.prevent="onLogin" class="w-full flex flex-col gap-3">
+          <Input
+            type="email"
+            placeholder="Email"
+            v-model="form.email"
+            class="w-full border border-gray-300 p-3 rounded-md text-black"
+            required
+          />
+            <div class="w-full flex items-center bg-white gap-3 border border-gray-300 p-3 rounded-md">
             <Input
-              type="email"
-              placeholder="Email"
-              v-model="form.email"
-              class="w-full border border-gray-300 p-3 rounded-md"
+              :type="show.password ? 'text' : 'password'"
+              placeholder="Password"
+              v-model="form.password"
+              class="bg-transparent outline-none flex-1 text-black"
               required
             />
-             <div class="w-full flex items-center bg-white gap-3 border border-gray-300 p-3 rounded-md">
-              <Input
-                :type="show.password ? 'text' : 'password'"
-                placeholder="Password"
-                v-model="form.password"
-                class="bg-transparent outline-none flex-1"
-                required
-              />
-              <span class="cursor-pointer" @click="isShow('password')">
-                  <EyeIcon v-if="!show.password" class="w-5 h-5"/>
-                  <EyeSlashIcon v-else class="w-5 h-5"/>
-              </span>
-            </div>
-            <Button
-              type="submit"
-              class="w-full bg-blue-500 rounded-md p-3 text-white hover:bg-blue-400 mt-3 "
-            >
-              Login
-            </Button>
-          </form>
-          <div class="flex flex-row justify-center gap-1">
-            <h5 class="text-center text-normal">Don't have an account?</h5>
-            <RouterLink to="/register" class="text-center text-blue-700 hover:underline">
-              Register
-            </RouterLink>
+            <span class="cursor-pointer" @click="isShow('password')">
+                <EyeIcon v-if="!show.password" class="w-5 h-5 text-black"/>
+                <EyeSlashIcon v-else class="w-5 h-5 text-black"/>
+            </span>
           </div>
-          <div class="flex flex-row justify-center gap-4 cursor-pointer">
-            <div class="cursor-pointer" @click="useGoogleLogin()">
-              <img
-                src="../assets/google.png"
-                alt="google"
-                width="25px"
-                height="25px"
-                class="object-contain"
-              />
-            </div>
-            <div class="cursor-pointer" @click="useFacebookLogin()">
-              <img
-                src="../assets/facebook.png"
-                alt="facebook"
-                width="25px"
-                height="25px"
-                class="object-contain"
-              />
-            </div>
-            <div class="cursor-pointer" @click="useAppleLogin()">
-              <img
-                src="../assets/apple.png"
-                alt="apple"
-                width="20px"
-                height="20px"
-                class="object-contain"
-              />
-            </div>
+          <Button
+            type="submit"
+            class="w-full bg-blue-500 rounded-md p-3 text-white hover:bg-blue-400 mt-3 "
+          >
+            Login
+          </Button>
+        </form>
+        <div class="flex flex-row justify-center gap-1">
+          <h5 class="text-center font-medium text-white">Don't have an account?</h5>
+          <RouterLink to="/register" class="text-center text-blue-700 hover:underline">
+            Register
+          </RouterLink>
+        </div>
+        <div class="flex flex-row justify-center gap-4 cursor-pointer">
+          <div class="cursor-pointer" @click="useGoogleLogin()">
+            <img
+              src="../assets/google.png"
+              alt="google"
+              width="25px"
+              height="25px"
+              class="object-contain"
+            />
+          </div>
+          <div class="cursor-pointer" @click="useFacebookLogin()">
+            <img
+              src="../assets/facebook.png"
+              alt="facebook"
+              width="25px"
+              height="25px"
+              class="object-contain"
+            />
+          </div>
+          <div class="cursor-pointer" @click="useAppleLogin()">
+            <img
+              src="../assets/apple.png"
+              alt="apple"
+              width="20px"
+              height="20px"
+              class="object-contain"
+            />
           </div>
         </div>
       </div>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -27,59 +27,56 @@ const onRegister = () => {
 </script>
 
 <template>
-  <div class="w-full h-full flex flex-row justify-center items-center">
-    <div class="h-full flex-1 ">
-      <div class="w-full h-full flex flex-col justify-center items-center">
-        <img src="../assets/signup.jpg" alt="signup" width="600" height="600" />
-      </div>
+  <div class="h-screen w-full bg-slate-950 flex flex-row text-slate-200 selection:bg-indigo-500/30 font-sans">
+    <div class="w-full h-full flex flex-col justify-center items-center">
+      <img src="../assets/signup.jpg" alt="signup" class="w-full h-full object-cover"/>
     </div>
-    <div class="h-full flex-1 bg-blue-300">
-      <div class="h-full flex flex-col gap-3 justify-center items-center">
-        <div class="w-[60%] flex flex-col gap-3 p-3">
-          <h1 class="text-2xl font-bold text-center">Form Register</h1>
-          <form @submit.prevent="onRegister" class="w-full flex flex-col gap-3">
+    <div class="w-full h-full flex flex-col justify-center items-center bg-blue-300">
+      <div class="w-[60%] flex flex-col gap-3 p-3">
+        <h1 class="text-2xl font-bold text-center text-white mb-4">Form Register</h1>
+        <form @submit.prevent="onRegister" class="w-full flex flex-col gap-3">
             <Input
               type="text"
               placeholder="Full Name"
               v-model="form.username"
-              class="w-full border border-gray-300 p-3 rounded-md"
+              class="w-full border border-gray-300 p-3 rounded-md text-black"
               required
             />
             <Input
               type="email"
               placeholder="Email"
               v-model="form.email"
-              class="w-full border border-gray-300 p-3 rounded-md"
+              class="w-full border border-gray-300 p-3 rounded-md text-black"
               required
             />
-            <div class="w-full bg-white gap-3 flex items-center border border-gray-300 p-3 rounded-md ">
+            <div class="w-full bg-white gap-3 flex items-center border border-gray-300 p-3 rounded-md text-black">
               <input
                 :type="show.password ? 'text' : 'password'"
                 placeholder="Password"
                 v-model="form.password"
-                class="bg-transparent outline-none flex-1"
+                class="bg-transparent outline-none flex-1 text-black"
                 required
               />
               <span class="cursor-pointer" @click="isShow('password')">
                 <span v-if="!show.password">
-                  <EyeIcon class="w-5 h-5"/>
+                  <EyeIcon class="w-5 h-5 text-black"/>
                 </span>
                 <span v-else>
-                  <EyeSlashIcon class="w-5 h-5"/>
+                  <EyeSlashIcon class="w-5 h-5 text-black"/>
                 </span>
               </span>
             </div>
-            <div class=" w-full flex items-center bg-white gap-3 border border-gray-300 p-3 rounded-md">
+            <div class=" w-full flex items-center bg-white gap-3 border border-gray-300 p-3 rounded-md text-black">
               <Input
                 :type="show.confirmPassword ? 'text' : 'password'"
                 placeholder="Confirm Password"
                 v-model="form.confirm_password"
-                class="bg-transparent outline-none flex-1"
+                class="bg-transparent outline-none flex-1 text-black"
                 required
               />
               <span class="cursor-pointer" @click="isShow('confirmPassword')">
-                  <EyeIcon v-if="!show.confirmPassword" class="w-5 h-5"/>
-                  <EyeSlashIcon v-else class="w-5 h-5"/>
+                  <EyeIcon v-if="!show.confirmPassword" class="w-5 h-5 text-black"/>
+                  <EyeSlashIcon v-else class="w-5 h-5 text-black"/>
               </span>
             </div>
             <Button
@@ -89,7 +86,7 @@ const onRegister = () => {
             >
           </form>
           <div class="flex flex-row justify-center gap-1">
-            <h5 class="text-center text-normal">Already have an account?</h5>
+            <h5 class="text-center font-medium text-white">Already have an account?</h5>
             <RouterLink to="/login" class="text-center text-blue-700 hover:underline">
               Login
             </RouterLink>
@@ -120,5 +117,4 @@ const onRegister = () => {
         </div>
       </div>
     </div>
-  </div>
 </template>
